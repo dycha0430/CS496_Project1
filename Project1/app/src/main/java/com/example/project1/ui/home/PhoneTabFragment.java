95eec868cee46548331cc07be176a9b54069535c
dycha0430 <68413811+dycha0430@users.noreply.github.com>
2021-12-29 13:02:14 +0900

    Recycler View implement

--- Project1/app/src/main/java/com/example/project1/ui/home/PhoneTabFragment.java
+++ Project1/app/src/main/java/com/example/project1/ui/home/PhoneTabFragment.java
@@ -11,32 +11,33 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.project1.R;
 import com.example.project1.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class PhoneTabFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    RecyclerView recyclerView;
+    PhoneTabAdapter phoneTabAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.phoneList);
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        phoneTabAdapter = new PhoneTabAdapter(getActivity());
+
+        recyclerView.setAdapter(phoneTabAdapter);
+
+        return rootView;
     }
+
+
 
     @Override
     public void onDestroyView() {
